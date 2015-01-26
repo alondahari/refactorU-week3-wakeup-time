9669fcd634fd947ccb918b53fdeb30adc8687c0e
Alon Dahari <climbinghobo@gmail.com>
2015-01-26 16:03:07 -0700

    implement build()

--- js/main.js
+++ js/main.js
@@ -16,15 +16,31 @@
       this._defaults = defaults;
       this._name = pluginName;
 
-      this.init();
+      init(this);
   }
 
-  Clock.prototype.init = function () {
-    this._build();
+  var init = function (theClock) {
+    build(theClock);
   };
 
-  Clock.prototype._build = function () {
-    this.$element.append('<div>');
+  var build = function (theClock) {
+    var outerShell = $('<div>').addClass('outer-shell'),
+        innerShell = $('<div>').addClass('inner-shell'),
+        pmLabel = $('<p>').addClass('pm-label'),
+        autoLabel = $('<p>').addClass('auto-label'),
+        clockScreen = $('<div>').addClass('clock-screen'),
+        pmIndicator = $('<div>').addClass('pm-indicator'),
+        autoIndicator = $('<div>').addClass('auto-indicator'),
+        clockText = $('<p>').addClass('clock-text'),
+        amFreq = $('<p>').addClass('am-freq'),
+        fmFreq = $('<p>').addClass('fm-freq');
+
+    //append all to the clock
+
+    clockScreen.append(autoLabel, pmIndicator, autoIndicator, clockText);
+    innerShell.append(pmLabel, autoLabel, clockScreen, amFreq, fmFreq);
+    outerShell.append(innerShell);
+    theClock.$element.append(outerShell);
   };
 
   // A really lightweight plugin wrapper around the constructor,
--- test/spec.js
+++ test/spec.js
@@ -5,5 +5,14 @@ describe('clock', function() {
 
   it('should append all elements to the DOM', function() {
     expect(fixture).toContainElement('.outer-shell');
+    expect(fixture.find('.outer-shell')).toContainElement('.inner-shell');
+    expect(fixture.find('.inner-shell')).toContainElement('.pm-label');
+    expect(fixture.find('.inner-shell')).toContainElement('.auto-label');
+    expect(fixture.find('.inner-shell')).toContainElement('.clock-screen');
+    expect(fixture.find('.clock-screen')).toContainElement('.pm-indicator');
+    expect(fixture.find('.clock-screen')).toContainElement('.auto-indicator');
+    expect(fixture.find('.clock-screen')).toContainElement('.clock-text');
+    expect(fixture.find('.inner-shell')).toContainElement('.am-freq');
+    expect(fixture.find('.inner-shell')).toContainElement('.fm-freq');
   });
 });
